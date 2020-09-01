176ae2086d8f883cf0743a40970c7f7d3cc91c24
KonradK2A <43847683+KonradK2A@users.noreply.github.com>
2020-09-01 18:29:45 +0200

    final commit
    filter enabled

--- shoppage.php
+++ shoppage.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="description" content="E-Kiermasz książek ZSŁ Gdańsk 2020">
-        <meta name="author" content="Krzysztof Madajczak / EwQ">
+        <meta name="author" content="Krzysztof Madajczak">
         <link rel="stylesheet" href="style.css">
         <title>Sklep - Kiermasz książek ZSŁ Gdańsk</title>
     </head>
@@ -12,30 +12,27 @@
             Witaj na stronie e-kiermaszu ZSŁ! <br> <br>
             <div class="menu">
                 <a href="mainpage.html">Strona główna</a>
-                <!-- <a href="shoppage.html">Sklep</a> -->
+                <!-- <a href="shoppage.php">Sklep</a> -->
                 <a href="addproductpage.html">Dodaj ofertę</a>
             </div>
         </div>
     <!-- This one displays filters -->
-    <form action="buyproduct.php" method="GET">
+    
         <div class="info">
             <div class="filters">
+            <form action="buyproduct.php" method="POST">
                 <select name="product_class" class="input">
                     <option value="" selected disabled hidden>Klasa</option>
-                    <option value="1">do klasy 1</option>
-                    <option value="2">do klasy 2</option>
-                    <option value="3">do klasy 3</option>
-                    <option value="4">do klasy 4</option>
-                </select>
-            </div>
-            <button id="button">Potwierdź</button>
+                    <option value="c1">do klasy 1</option>
+                    <option value="c2">do klasy 2</option>
+                    <option value="c3">do klasy 3</option>
+                    <option value="c4">do klasy 4</option>
+                </select> <br>
+                <input type="submit" value="Potwierdź"> 
+            </form> 
         </div>
-    </form>        
-    <!-- This one displays options that match filters -->
-    <div class="info">
-        <?php include "buyproduct.php";
-            showall();
-        ?>
-    </div>
+        
+    </div>           
+    
     </body>
 </html>
